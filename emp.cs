e385e532d9c2a41204b0092f76d6b41e81ebcb4d
NIRAJ <niraj@rajniraj.com>
2023-05-19 18:08:01 +0530

    [Niraj] Added :calculate monthly wage

--- emp.cs
+++ emp.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualBasic;
 using System.Drawing;
+using System.Globalization;
 
 namespace ConsoleApp1
 {
@@ -8,36 +9,46 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("This is the employee wage program");
-            Random rd = new Random();
-            int attendance = rd.Next(0, 3);
+
+           
             int salary = 0;
             const int WagePerHour = 20;
             const int present = 0;
             const int absent = 1;
             const int month = 20;
             int dailyHour = 0;
+            const int TotalWorkingDays = 20;
+            const int MaxWorkingHours = 100;
 
-
-            switch (attendance)
+            for (int i = 0; i <= TotalWorkingDays; i++)
             {
-                case present:
-                    Console.WriteLine("The employee is present full time  ");
-                    dailyHour = 8;
-                    break;
-                case absent:
-                    Console.WriteLine("The employee is absent ");
-                    dailyHour = 0;
-                    break;
-                default:
-                    Console.WriteLine("The employee is present part time ");
-                    dailyHour = 4;
-                    break; 
+                for (int j = 0; j <= MaxWorkingHours; j++) ;
+                {
+                    Random rd = new Random();
+                    int attendance = rd.Next(0, 3);
+
+                    switch (attendance)
+                    {
+                        case present:
+                            Console.WriteLine("The employee is present full time  ");
+                            dailyHour = 8;
+                            break;
+                        case absent:
+                            Console.WriteLine("The employee is absent ");
+                            dailyHour = 0;
+                            break;
+                        default:
+                            Console.WriteLine("The employee is present part time ");
+                            dailyHour = 4;
+                            break;
+                    }
+
+                    salary = dailyHour * WagePerHour;
+                    Console.WriteLine("The salary of the emloyee is :  " + salary + " /- rs");
+
+                    Console.WriteLine("The monthly salary of the employee is :  " + month * salary + " /- rs");
+                }
             }
-
-            salary = dailyHour * WagePerHour;
-            Console.WriteLine("The salary of the emloyee is :  " + salary + " /- rs");
-
-            Console.WriteLine("The monthly salary of the employee is :  " + month * salary + " /- rs");
-        }
+        }       
     }
 }
