353b26d9a618eb1ec5612f0bb7f1235920e5ed01
NIRAJ <niraj@rajniraj.com>
2023-05-19 18:05:23 +0530

    [Niraj] Added :program using switch case

--- emp.cs
+++ emp.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic;
+using System.Drawing;
+
 namespace ConsoleApp1
 {
     internal class Program
@@ -6,35 +9,35 @@ namespace ConsoleApp1
         {
             Console.WriteLine("This is the employee wage program");
             Random rd = new Random();
-            int attendance=rd.Next(0,3);
+            int attendance = rd.Next(0, 3);
             int salary = 0;
             const int WagePerHour = 20;
             const int present = 0;
             const int absent = 1;
+            const int month = 20;
             int dailyHour = 0;
 
 
-            if (attendance == present )
+            switch (attendance)
             {
-                Console.WriteLine("Employee is present");
-                dailyHour = 8;
-                
+                case present:
+                    Console.WriteLine("The employee is present full time  ");
+                    dailyHour = 8;
+                    break;
+                case absent:
+                    Console.WriteLine("The employee is absent ");
+                    dailyHour = 0;
+                    break;
+                default:
+                    Console.WriteLine("The employee is present part time ");
+                    dailyHour = 4;
+                    break; 
             }
 
-            else if (attendance == absent ) 
-            {
-                Console.WriteLine("Employee is absent");
-                dailyHour = 0;
-            }
-            else
-            {
-                Console.WriteLine("Employee is present part time ");
-                dailyHour = 4;
-            }
-            
             salary = dailyHour * WagePerHour;
-            Console.WriteLine("The salary of the emloyee is :  " + salary );
+            Console.WriteLine("The salary of the emloyee is :  " + salary + " /- rs");
 
+            Console.WriteLine("The monthly salary of the employee is :  " + month * salary + " /- rs");
         }
     }
-}   
+}
