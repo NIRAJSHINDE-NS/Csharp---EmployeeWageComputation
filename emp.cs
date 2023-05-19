f10a14868f3ddcfd9b5ed941205a503caf133065
NIRAJ <niraj@rajniraj.com>
2023-05-19 18:03:44 +0530

    [Niraj] Added : This is the program to calculate part time salary

--- emp.cs
+++ emp.cs
@@ -6,24 +6,35 @@ namespace ConsoleApp1
         {
             Console.WriteLine("This is the employee wage program");
             Random rd = new Random();
-            int attendance=rd.Next(0,2);
+            int attendance=rd.Next(0,3);
             int salary = 0;
-            const int FullDayHour = 8;
             const int WagePerHour = 20;
+            const int present = 0;
+            const int absent = 1;
+            int dailyHour = 0;
 
 
-            if (attendance == 0 )
+            if (attendance == present )
             {
                 Console.WriteLine("Employee is present");
-                salary = FullDayHour * WagePerHour;
-                Console.WriteLine("The salary of the emloyee is :  " + salary);
+                dailyHour = 8;
+                
             }
-            else 
+
+            else if (attendance == absent ) 
             {
                 Console.WriteLine("Employee is absent");
-                Console.WriteLine("salary of the employee is :  " + salary );
+                dailyHour = 0;
             }
+            else
+            {
+                Console.WriteLine("Employee is present part time ");
+                dailyHour = 4;
+            }
+            
+            salary = dailyHour * WagePerHour;
+            Console.WriteLine("The salary of the emloyee is :  " + salary );
 
         }
     }
-}
+}   
