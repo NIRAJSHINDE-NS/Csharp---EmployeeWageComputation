89c804594635c33dbe1e814e90c2d501096510b5
NIRAJ <niraj@rajniraj.com>
2023-05-19 17:58:18 +0530

    [Niraj] Added : Employee Wage to check attendance

---
+++ emp.cs
@@ -0,0 +1,25 @@
+namespace EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            Console.WriteLine("--------------------------------------------");
+
+            Random random = new Random();
+            int attendance = random.Next(0, 2);
+
+            //Checking if the Employee is Present or not
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
